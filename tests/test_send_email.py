9f419e3c672434be5f03583816da9fb5d0d24492
Carol <naousual@gmail.com>
2019-11-05 18:12:04 -0300

    chamando o script sem função

--- tests/test_send_email.py
+++ tests/test_send_email.py
@@ -5,7 +5,7 @@ import send_mail
 def test_mail_was_sent(mocker):
     mocker.patch('send_mail.smtplib.SMTP')
     sendmail_mock = mocker.patch('send_mail.smtplib.SMTP.sendmail')
-
+    exec(open("send_mail.py").read())
     assert sendmail_mock.called
 
 
