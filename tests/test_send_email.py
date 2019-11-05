b5d1657a9fe7bb4c1c1a7c6e196b079808557ab7
Carol <naousual@gmail.com>
2019-11-05 16:41:55 -0300

    first test

--- tests/test_send_email.py
+++ tests/test_send_email.py
@@ -2,4 +2,11 @@ import pytest
 import send_mail
 
 
+def test_mail_was_sent(mocker):
+    mocker.patch('send_mail.smtplib.SMTP')
+    sendmail_mock = mocker.patch('send_mail.smtplib.SMTP.sendmail')
+
+    assert sendmail_mock.called
+
+
 
